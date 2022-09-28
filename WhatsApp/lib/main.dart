0beaf7b61c812e9067fe3a60b89b915e82121328
ahmedalgzery <algzeryahmed14@gmail.com>
2022-09-28 23:06:19 +0200

    update sdk

--- WhatsApp/lib/main.dart
+++ WhatsApp/lib/main.dart
@@ -1,6 +1,8 @@
+
 import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
 import 'package:screens/layout/homescreen.dart';
+
 List<CameraDescription> ?cameras;
 void main()  {
   
